bd0d6738bbc3b901c0cc046ceedc34c96458301d
Ryan Giordano <rgiordan@gmail.com>
2016-08-30 16:13:10 -0700

    First pass through trim source tiles

--- test/test_elbo_values.jl
+++ test/test_elbo_values.jl
@@ -407,8 +407,15 @@ function test_trim_source_tiles()
     # With the above seed, this is near the middle of the image.
     s = 1
     ea.active_sources = [s]
-    ea42 = deepcopy(ea)
-    Infer.trim_source_tiles!(ea42; noise_fraction=0.1);
+    ea_trimmed = deepcopy(ea);
+    Infer.trim_source_tiles!(ea_trimmed; noise_fraction=0.1);
+
+    b = 3
+    img = deepcopy(blob[b].pixels);
+    img[img .> 1000] = 1000;
+    m_pos = WCSUtils.world_to_pix(ea.patches[s, b].wcs_jacobian,
+                                  ea.patches[s, b].center,
+                                  ea.patches[s, b].pixel_center, bodies[s].pos)
 
     loc_ids = ids.u
     non_loc_ids = setdiff(1:length(ids), ids.u)
@@ -416,15 +423,12 @@ function test_trim_source_tiles()
         println("Testing b = $b")
         # Make sure pixels got NaN-ed out
         @test(
-            sum([ sum(!Base.isnan(tile.pixels)) for tile in ea42.images[b].tiles]) <
+            sum([ sum(!Base.isnan(tile.pixels)) for tile in ea_trimmed.images[b].tiles]) <
             sum([ sum(!Base.isnan(tile.pixels)) for tile in ea.images[b].tiles]))
         s_tiles = find_source_tiles(s, b, ea)
         ea.active_sources = [s];
+        elbo_trim = ElboDeriv.elbo(ea_trimmed; calculate_hessian=false);
         elbo_full = ElboDeriv.elbo(ea; calculate_hessian=false);
-
-        ea_trimmed = deepcopy(ea)
-        ea_trimmed.images = trimmmed_tiled_blob
-        elbo_trim = ElboDeriv.elbo(ea2; calculate_hessian=false);
         @test_approx_eq_eps(
             elbo_full.d[loc_ids, 1] ./ elbo_trim.d[loc_ids, 1],
             fill(1.0, length(loc_ids)), 0.06)
@@ -435,38 +439,35 @@ function test_trim_source_tiles()
 
     # Test min_radius_pix on just one tile.
     b = 3
-    s_tiles = find_source_tiles(s, b, ea)
+    s_tiles = find_source_tiles(s, b, ea);
 
     # Set the source to be very dim:
-    ea.vp[s][ids.r1] = 0.01
-    ea.vp[s][ids.r2] = 0.01
+    ea.vp[s][ids.r1] = 0.001
+    ea.vp[s][ids.r2] = 0.001
 
     min_radius_pix = 6.0
-    ea43 = deepcopy(ea42)
-    Infer.trim_source_tiles!(ea43, noise_fraction=0.1, min_radius_pix = min_radius_pix);
-    trimmed_tiled_blob = ea43.images
+    ea_trimmed = deepcopy(ea);
+    Infer.trim_source_tiles!(ea_trimmed, noise_fraction=0.1, min_radius_pix = min_radius_pix);
 
+    # TODO: this is currently failing.
     total_nonempty_pixels = 0.0
     for tile_index in s_tiles
-        tile = trimmed_tiled_blob[b].tiles[tile_index...]
+        tile = ea_trimmed.images[b].tiles[tile_index...]
         total_nonempty_pixels += sum(!Base.isnan(tile.pixels))
     end
     @test_approx_eq_eps total_nonempty_pixels pi * min_radius_pix ^ 2 2.0
 
     min_radius_pix = 0.0
-    ea44 = deepcopy(ea)
+    ea_trimmed = deepcopy(ea);
     Infer.trim_source_tiles!(
-                s, ea44, trimmed_tiled_blob, noise_fraction=0.1,
-                        min_radius_pix = min_radius_pix);
-    trimmed_tiled_blob = ea44.images
+        ea_trimmed, noise_fraction=0.1, min_radius_pix = min_radius_pix);
 
     total_nonempty_pixels = 0.0
     for tile_index in s_tiles
-        tile = trimmed_tiled_blob[b].tiles[tile_index...]
+        tile = ea_trimmed.images[b].tiles[tile_index...]
         total_nonempty_pixels += sum(!Base.isnan(tile.pixels))
     end
     @test total_nonempty_pixels == 0.0
-
 end
 
 
